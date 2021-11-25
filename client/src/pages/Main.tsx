58dc3892af81a32ac248e4a7077d9d886bc76151
Jonathan <71105258+JAruliah@users.noreply.github.com>
2021-11-24 22:35:42 -0700

    external api for words

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,5 +1,5 @@
+//https://api.quotable.io/random?minLength=150
 import React, {useState, useEffect, useRef} from 'react'
-import randomWords from 'random-words'
 import {Content} from '../components/Content'
 import {Header} from '../components/Header'
 import { MDBBtn } from 'mdb-react-ui-kit'
@@ -13,9 +13,9 @@ interface MainProps {
 }
 
 // constants for number of generated words and seconds for the game timer
-const NUMB_OF_WORDS:number = 150
-const SECONDS:number = 60
-
+const SECONDS:number = 120
+// global interval for the game timer
+var interval:NodeJS.Timeout
 export const Main: React.FC<MainProps> = ({userEmail, isAuthenticated,setScores, scores, user}) => {
     const [words, setWords] = useState<string[]>([])
     const [countDown, setCountDown] = useState<number>(SECONDS)
@@ -30,10 +30,12 @@ export const Main: React.FC<MainProps> = ({userEmail, isAuthenticated,setScores,
     const [startCountDown, setStartCountDown] = useState<number>(-1)
     const [accuracyScore, setAccuracyScore] = useState<number>(0)
     const textInput = useRef<null|any>(null)
+    const [wpm, setWpm] = useState<number>(0)
+
 
     // generate random on mount
     useEffect(() => {
-        setWords(generateWords())
+        generateWords()
     }, [])
 
     // when the game status changes check if the game is started
@@ -43,6 +45,7 @@ export const Main: React.FC<MainProps> = ({userEmail, isAuthenticated,setScores,
         if (status === "start"){
             textInput.current.focus()
         }
+        // If the game is finished post the stats to the db
         if (status === "finished"){
             setAccuracyScore((correctChar / (correctChar + incorrectChar) * 100))
             if (isAuthenticated){
@@ -54,7 +57,7 @@ export const Main: React.FC<MainProps> = ({userEmail, isAuthenticated,setScores,
                                 headers:{
                                     'Content-Type': 'application/json'
                                 },
-                                body: JSON.stringify({email:userEmail,score:completedWords, accuracy:accuracyScore})
+                                body: JSON.stringify({email:userEmail,score:wpm, accuracy:accuracyScore})
                             })
                             .then(res => res.json())
                             .then(data => {
@@ -63,33 +66,52 @@ export const Main: React.FC<MainProps> = ({userEmail, isAuthenticated,setScores,
         
                             })
                             .catch(err => console.log(err))
-    
                         }
                     }
-
                 }
             }
         }
         return () => { isMounted = false };
+    }, [status,userEmail,completedWords, isAuthenticated, setScores, accuracyScore, incorrectChar, correctChar, wpm])
 
-    }, [status,userEmail,completedWords, isAuthenticated, setScores, accuracyScore, incorrectChar, correctChar])
+    // fetch words from an api to be used as the typing content
+    const generateWords = () :void => {
+        fetch(`https://api.quotable.io/random?minLength=150`)
+        .then(res => res.json())
+        .then(data => {
+            let words = data.content
+            words = words.split(' ')
 
-    // generate random words for the typing content 
-    const generateWords = () :string[] => {
-        return new Array(NUMB_OF_WORDS).fill(null).map(() => randomWords()+" ")
+            let newWords:string[] = []
+            words.forEach((word:string, index:number) => {
+                newWords[index] = word + " "
+            })
+            setWords(newWords)
+        })
+        .catch(err => {console.log(err)})
+
+    }
+
+    // function to calculate the wpm at the end of a game
+    const calculateWPM = () => {
+        let time = SECONDS - countDown
+        let timeMinutes = time / 60
+        return (completedWords / timeMinutes)
     }
 
     // start button, starts interval and sets coundown state
     const start = ():number|void => {
         // if the game is finished reset all values
         if (status === "finished" || status === "post-game"){
-            setWords(generateWords())
+            generateWords()
             setCurrentWordIndex(0)
             setCurrentCharIndex(0)
             setCurrentChar([])
             setCompletedWords(0)
             setCorrectChar(0)
             setIncorrectChar(0)
+            setWpm(0)
+            setAccuracyScore(0)
 
         }
         // change game status to start
@@ -103,11 +125,13 @@ export const Main: React.FC<MainProps> = ({userEmail, isAuthenticated,setScores,
                     // when the start countdown reaches 0 start the game
                     if(prevStartCountDown === 0){
                         clearInterval(startInvertal)
+                        setCountDown(SECONDS)
                         setStatus("start")
-                        let interval = setInterval(() => {
+                            interval = setInterval(() => {
                             setCountDown((prevCountDown) => {
                                 // if the countdown hits 0 clear the interval and set status to finished
                                 if(prevCountDown === 0){
+                                    setWpm(calculateWPM())
                                     setStatus("finished")
                                     clearInterval(interval)
                                     setCurrentInput("")
@@ -128,9 +152,11 @@ export const Main: React.FC<MainProps> = ({userEmail, isAuthenticated,setScores,
     // on space keydown check if the word matches the current word, if it does move to next word and clear input
     const handleKeyDown = ({key} :React.KeyboardEvent<HTMLInputElement>) => {
         // if the key is not a backspace append the key to the current char array
+        if (key === "Shift"){
+            return
+        }
         if (key !== "Backspace"){
             currentChar.push(key)
-
         }
         // if the key is a backspace pop off the current char array and decrease currentCharIndex by 1
         if (key === "Backspace"){
@@ -151,11 +177,20 @@ export const Main: React.FC<MainProps> = ({userEmail, isAuthenticated,setScores,
             if (key === " "){
                 // if the key down if a space check if word matches, move to next word if it does
                 if(words[currentWordIndex] === currentInput.trim()+" "){
+                    // if the last line is completed, change game status and clear interval
+                    if(currentWordIndex+1 === words.length){
+                        setWpm(calculateWPM())
+                        setStatus("finished")
+                        clearInterval(interval)
+                        setCurrentInput("")
+                    }
+                    // if not the end of the game continue
                     setCurrentInput("")
                     setCurrentWordIndex(currentWordIndex + 1)
                     setCurrentCharIndex(0)
                     setCurrentChar([])
                     setCompletedWords(completedWords + 1)
+                    
                 }
             }
                 // if key is not a space, move the index up by one 
@@ -167,6 +202,8 @@ export const Main: React.FC<MainProps> = ({userEmail, isAuthenticated,setScores,
         }else if (key !== words[currentWordIndex][currentCharIndex]){ 
             setIncorrectChar(incorrectChar+1)
         }
+
+
     }
 
         return (
@@ -176,12 +213,12 @@ export const Main: React.FC<MainProps> = ({userEmail, isAuthenticated,setScores,
                     {status === "start" || status === "post-game" ? 
                         <div className="d-flex justify-content-center mt-3">
                             <div className="text-center mx-3">{correctChar === 0 && incorrectChar === 0 ? <h3>Accuracy: 0%</h3>:<h3>Accuracy: {Math.round((correctChar / (correctChar + incorrectChar) * 100))}%</h3>}</div>
-                            <div className="text-center mx-3"><h3>WPM: {completedWords}</h3></div>
+                            <div className="text-center mx-3"><h3>WPM: {Math.round(calculateWPM())}</h3></div>
                         </div>
                     :null }
                     <div className="timer text-center">
                         {startCountDown !== -1 ? <h3 className="text-danger" >Starts in: {startCountDown}</h3>: null}
-                        {status === "start" ? <h3>{countDown}</h3>: null}
+                        {status === "start"|| status === "post-game" ? <h3>{countDown}</h3>: null}
                     </div>
                     {status === "start" || status === "start-timer" ? <Content words={words} contentStyle={""} currentCharIndex={currentCharIndex} currentWordIndex={currentWordIndex}/>
                     : 
